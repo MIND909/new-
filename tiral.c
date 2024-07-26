34634720327fee7dd031adea6f3317e31721b2c7
MIND909 <mind99323@gmail.com>
2024-07-26 14:31:42 +0530

    update

---
+++ tiral.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+     int num1,num2,;
+
+//addition 
+prinftf("\nEnter the value of num1 = ");
+scanf("%d ,&num1);
+printf("\nEnter the value of num2 =");
+Scanf("%d,&num2);
+Printf("\nThe addition of %d and %d is %d" , num1,num2,num1+num2);
+
+//subtraction 
+Prinftf("\nEnter the value of num1=");
+Scanf("%d ,&num1);
+Printf("\nEtner the value of num2=");
+Scanf("%d ,&num2);
+Printf("\nThe subtraction of %d and %d is %d" ,num1,num2,num1-num2);
+
+//multiplication 
+Prinftf("\nEnter the value of num1=");
+Scanf("%d ,&num1);
+Printf("\nEtner the value of num2=");
+Scanf("%d ,&num2);
+Printf("\nThe multiplication of %d and %d is %d" ,num1,num2,num1*num2);
+
+//division
+Prinftf("\nEnter the value of num1=");
+Scanf("%d ,&num1);
+Printf("\nEtner the value of num2=");
+Scanf("%d ,&num2);
+Printf("\nThe division of %d and %d is %d" ,num1,num2,num1/num2);
+
+//remainder 
+Printf("\nEnter the value of num1=");
+Scanf("d ,&num1);
+Printf("\nEtner the value of num2=");
+Scanf("%d ,&num2);
+Printf("\nThe remainder  of %d and %d is %d",num1,num2,num1%num2);
+ 
+ return 0;
+ 
+}
